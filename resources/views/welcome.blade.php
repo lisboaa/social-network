402496b3869ea046b3949695f24aa6517c7804ba
Douglas <douglas.lisboa@edu.unipar.br>
2019-08-26 10:18:50 -0300

    Implementado template iniciado do formulario.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,38 @@
 @endsection
 
 @section('content')
-    Welcome! Boy
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Sing In</h3>
+            <form action="post">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="email">Your First name</label>
+                    <input class="form-control" type="text" name="first_name" id="first_name">
+                </div>
+                <div class="form-group">
+                    <label for="email">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <div class="col-md-6">
+            <h3>Sin In</h3>
+            <form action="post">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="email">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
 @endsection
