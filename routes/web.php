1575085aa43c1987d8761ec86afe87142bde6ef4
Douglas <douglas.lisboa@edu.unipar.br>
2019-08-26 11:51:29 -0300

    Salvando informações do form no banco.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::post('/singup', [
+        'uses' => 'UserController@postSingUp',
+        'as' => 'singup'
+    ]);
 });
